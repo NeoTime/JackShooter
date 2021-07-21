47609167e693d32f82feee410899a59f7794aedb
Jefferey <riglerjeff@gmail.com>
2021-07-21 12:56:56 -0700

    Update ShooterAnimInstance
    See with added Root bone work.

--- Source/JackShooter/ShooterAnimInstance.h
+++ Source/JackShooter/ShooterAnimInstance.h
@@ -14,10 +14,17 @@ class JACKSHOOTER_API UShooterAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 public:
+	UShooterAnimInstance();
+
 	UFUNCTION(BlueprintCallable)
 	void UpdateAnimationProperties(float DeltaTime);
 
 	virtual void NativeInitializeAnimation() override;
+
+protected:
+
+	// Handle turn in place variables
+	void TurnInPlace();
 
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
@@ -34,5 +41,33 @@ private:
 	// Whether or not moving
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	bool bIsAccelerating;
-	
+
+	// Offset Yaw Used for Straifing
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement, meta = (AllowPrivateAccess = "true")) 
+	float MovementOffsetYaw;
+
+	// Offset Yaw the Frame before we stopped moving
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float LastMovementOffsetYaw;
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bAiming;
+
+	// Yaw of Character this frame 
+	float CharacterYaw;
+
+	// Yaw of character's previous frame
+	float CharacterYawLastFrame;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Turn in Place", meta = (AllowPrivateAccess = "true"))
+	float RootYawOffset;
+
+	// The Pitch of aim rotation used for Aim Offset
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Turn in Place", meta = (AllowPrivateAccess = "true"))
+	float Pitch;
+
+	// True when Reloading used to prevent aim offset while reloading
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Turn in Place", meta = (AllowPrivateAccess = "true"))
+	bool bReloading;
 };
